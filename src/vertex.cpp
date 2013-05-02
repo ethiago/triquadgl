47b53155f3c109c0e45eb871f67bff3fb78396ab
e.thiago.06 <e.thiago.06@93bfcffd-98b1-f761-c181-7832c6f007b5>
2013-05-02 22:23:11 +0000

    CHE implementado, mas nao incorporado

---
+++ src/vertex.cpp
@@ -0,0 +1,53 @@
+#include "vertex.h"
+
+Vertex::Vertex(float x , float y )
+{
+    m_x = x;
+    m_y = y;
+}
+
+Vertex::Vertex(const Vertex& v)
+{
+    m_x = v.x();
+    m_y = v.y();
+    m_halfedgeIndex = v.halfedgeIndex();
+}
+
+float Vertex::x()const
+{
+    return m_x;
+}
+
+float Vertex::y()const
+{
+    return m_y;
+}
+
+int  Vertex::halfedgeIndex()const
+{
+    return m_halfedgeIndex;
+}
+
+float& Vertex::x()
+{
+    return m_x;
+}
+
+float& Vertex::y()
+{
+    return m_y;
+}
+
+int&  Vertex::halfedgeIndex()
+{
+    return m_halfedgeIndex;
+}
+
+const Vertex& Vertex::operator=(const Vertex& v)
+{
+    m_x = v.x();
+    m_y = v.y();
+    m_halfedgeIndex = v.halfedgeIndex();
+
+    return *this;
+}
